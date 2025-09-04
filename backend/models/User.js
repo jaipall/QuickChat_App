b3d7cd2eac818a6ccefbdfbce0a7367c423a159d
Jaipall <singhjaipal210503@gmail.com>
2025-09-05 00:32:51 +0530

    chh

--- backend/models/User.js
+++ backend/models/User.js
@@ -11,6 +11,6 @@ const userSchema = new mongoose.Schema(
   { timestamps: true }
 );
 
-const User = mongoose.model("User", userSchema);
+const User = mongoose.model("chatUsers", userSchema);
 
 export default User;
